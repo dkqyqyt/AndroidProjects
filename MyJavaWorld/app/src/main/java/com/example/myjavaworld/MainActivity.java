859e59d53bc8905251526533de31d8b7beade700
dkqyqyt <rsm4282@naver.com>
2020-03-28 19:24:51 +0900

    Add First Project

---
+++ MyJavaWorld/app/src/main/java/com/example/myjavaworld/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.myjavaworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.myjavaworld.MESSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**Called when the user taps the Send button*/
+    public void sendMessage(View view){
+        // Do something in respnse to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText2);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+}
