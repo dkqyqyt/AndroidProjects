d59ffc7799e261f8eed2b3943990aa2e924c7697
dkqyqyt <rsm4282@naver.com>
2020-04-04 18:08:25 +0900

    Add 진행도를 조절하는 메소드

--- ProgressBar/app/src/main/java/com/example/progressbar/MainActivity.java
+++ ProgressBar/app/src/main/java/com/example/progressbar/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.progressbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    // 뷰의 주소값을 담을 참조 변수
+    ProgressBar bar4;
+   @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bar4 = (ProgressBar)findViewById(R.id.progressBar4);
+    }
+
+    public void btn1Method(View view){
+       bar4.incrementProgressBy(5);
+    }
+
+    public void btn2Method(View view){
+       bar4.incrementProgressBy(-5);
+    }
+
+    public void btn3Method(View view){
+       bar4.setProgress(80);
     }
 }
