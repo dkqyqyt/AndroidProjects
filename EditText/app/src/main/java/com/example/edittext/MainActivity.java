1447ed76d17f996e784906304dd1caf267a6c53e
dkqyqyt <rsm4282@naver.com>
2020-04-05 13:24:56 +0900

    Add 엔터키를 눌렀을 때 반응하는 리스너

--- EditText/app/src/main/java/com/example/edittext/MainActivity.java
+++ EditText/app/src/main/java/com/example/edittext/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.edittext;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -19,9 +22,30 @@ public class MainActivity extends AppCompatActivity {
 
         edit1 = (EditText)findViewById(R.id.editText2);
         text1 = (TextView)findViewById(R.id.textView);
+
+        EnterListener listener = new EnterListener();
+        edit1.setOnEditorActionListener(listener);
     }
 
     public void btn1Method(View view){
         edit1.setText("새롭게 설정한 문자열");
     }
+
+    public void btn2Method(View view){
+        String str = edit1.getText().toString();
+        text1.setText("입력한 문자열 : " + str);
+    }
+
+    class EnterListener implements TextView.OnEditorActionListener{
+        @Override
+        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+
+            String str = v.getText().toString();
+
+            text1.setText("입력한 문자열 : " + str);
+
+            return true;
+        }
+    }
+
 }
