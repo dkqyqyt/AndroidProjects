0d58b3d8f67f09b851dbb8fdc84157d0eaf98bee
dkqyqyt <rsm4282@naver.com>
2020-04-05 14:16:28 +0900

    Add 이미지를 설정하는 메소드

--- ImageView/app/src/main/java/com/example/imageview/MainActivity.java
+++ ImageView/app/src/main/java/com/example/imageview/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.imageview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // 뷰의 주소값을 담을 참조변수
+    ImageView img3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 뷰의 주소값을 받아온다.
+        img3 = (ImageView)findViewById(R.id.imageView3);
+        // 이미지를 세팅한다.
+        img3.setImageResource(R.drawable.img_android);
     }
 }
